2a9558428a912db0b5a52c72118933c3019a1cfd
ma7m00dma7m00d <mahmoudmahmoud20000101@gmail.com>
2023-03-05 23:48:29 +0300

    Add custom event implementation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,86 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.ComponentModel;
+
+namespace Events
+{
+    public class MyClass
+    {
+        protected EventHandlerList _subs;
+
+        public event EventHandler SomeEvent
+        {
+            add
+            {
+                _subs.AddHandler(this, value);
+            }
+            remove
+            {
+                _subs.RemoveHandler(this, value);
+            }
+        }
+
+    }
+
+    public class MyCustomEventArgs : EventArgs
+    {
+        public int Count { set; get; }
+    }
+
+    public class Events
+    {
+        private int count = 0;
+
+        public EventHandler<MyCustomEventArgs> onCountIncrement;
+        public EventHandler<MyCustomEventArgs> onCountDecrement;
+
+        public EventHandler onSomething;
+
+        public int Count { get => count; }
+
+        public Events()
+        {
+        }
+
+        public void Increment()
+        {
+            count++;
+            if (onCountDecrement != null)
+            {
+                onCountIncrement(this, new MyCustomEventArgs { Count = count });
+            }
+        }
+
+        public void Decrement()
+        {
+            count--;
+            if (onCountDecrement != null)
+            {
+                onCountDecrement(this, new MyCustomEventArgs { Count = count });
+            }
+        }
+    }
+
+    public static class Program
+    {
+        public static void Main(string[] args)
+        {
+            Events events = new Events();
+            System.Console.WriteLine(events.Count);
+            events.onCountIncrement += PrintIncrement;
+            events.onCountDecrement += PrintDecrement;
+
+            events.Increment();
+            events.Increment();
+            events.Decrement();
+        }
+
+        public static void PrintIncrement(object sender, MyCustomEventArgs e)
+        {
+            System.Console.WriteLine($"Incremented now count is: {e.Count}");
+        }
+
+        public static void PrintDecrement(object sender, MyCustomEventArgs e)
+        {
+            System.Console.WriteLine($"Decremented now count is: {e.Count}");
+        }
+    }
+}
